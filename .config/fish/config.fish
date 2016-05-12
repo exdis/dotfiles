ee440787cb3100a87a4932a45a09e381c5305d9c
Denis Kolesnikov <exsdis@gmail.com>
2016-05-12 20:04:14 +0600

    fish config changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
 # Path to your oh-my-fish.
-set -gx OMF_PATH "/home/exdis/.local/share/omf"
+set -gx OMF_PATH "$HOME/.local/share/omf"
 
 # Customize Oh My Fish configuration path.
-set -gx OMF_CONFIG "/home/exdis/.config/omf"
+set -gx OMF_CONFIG "$HOME/.config/omf"
 
 # Load oh-my-fish configuration.
 source $OMF_PATH/init.fish
