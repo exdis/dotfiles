e2a472bf2c9513b00eb23c13087d24a7fe6ea10c
Denis Kolesnikov <exsdis@gmail.com>
2024-01-16 14:47:02 +0100

    Prettier changes

--- .config/nvim/lua/neoformat-config.lua
+++ .config/nvim/lua/neoformat-config.lua
@@ -3,7 +3,19 @@ vim.g['prettier#autoformat_require_pragma'] = 0
 vim.g['prettier#autoformat_config_present'] = 1
 
 -- NeoFormat
-vim.g[':neoformat_try_node_exe'] = 1
+vim.g['neoformat_try_node_exe'] = 1
+
+vim.cmd [[
+let g:neoformat_javascript_prettier = {
+  \ 'exe': 'node_modules/.bin/prettier',
+  \ 'args': ['--stdin', '--stdin-filepath', '"%:p"'],
+  \ 'stdin': 1,
+  \ 'try_node_exe': 1,
+  \ }
+
+let g:neoformat_enabled_javascript = ['prettier']
+]]
+
 vim.cmd [[
   autocmd BufWritePre *.js Neoformat
   autocmd BufWritePre *.jsx Neoformat
