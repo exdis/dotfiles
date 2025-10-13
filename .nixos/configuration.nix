47e17a8fe3a893d4f5514c4d432697258207eb7a
Denis Kolesnikov <exdis@proton.me>
2025-10-13 18:25:44 +0000

    NixOS current changes

--- .nixos/configuration.nix
+++ .nixos/configuration.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 {
   imports =
@@ -7,7 +7,15 @@
     ];
 
   boot.loader.systemd-boot.enable = true;
-  boot.kernelModules = [ "vboxguest" "vboxsf" "vboxvideo" ];
+  boot.kernelModules = [ "vboxguest" "vboxsf" "vboxvideo" "uinput" ];
+
+  hardware.uinput.enable = true;
+
+  users.groups.uinput = { };
+
+  services.udev.extraRules = ''
+    KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+  '';
 
   users.users.exdis = {
     isNormalUser = true;
@@ -15,8 +23,23 @@
     shell = pkgs.fish;
   };
 
-  hardware.graphics.enable = true;
-  hardware.graphics.extraPackages = with pkgs; [ libglvnd ];
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [ libglvnd ];
+  };
+
+  services.xserver = {
+    videoDrivers = [ "nvidia" ];
+    xkb.layout = "us,ru";
+  };
+
+  console.keyMap = "us";
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = true;
+    nvidiaSettings = true;
+  };
 
   programs.fish.enable = true;
   programs.neovim = {
@@ -30,30 +53,69 @@
     xwayland.enable = true;
   };
 
+  # programs.uwsm.enable = true;
+  # programs.uwsm.waylandCompositors.hyprland = {
+  #   prettyName = "Hyprland";
+  #   comment = "Hyprland compositor managed by UWSM";
+  #   binPath = "/run/current-system/sw/bin/Hyprland";
+  # };
+  #
+  # services.displayManager.sddm.enable = true;
+  # services.displayManager.sddm.wayland.enable = true;
+  # services.displayManager.defaultSession = "hyprland-uwsm";
+
+  programs.firefox.enable = true;
+
   networking.networkmanager.enable = true;
 
   environment.systemPackages = with pkgs; [
-    git
-    nodejs
-    pyenv
-    cargo
-    rustc
-    zig
-    diff-so-fancy
-    fzf
-    zoxide
-    eza
-    ripgrep
+    inputs.zen-browser.packages.x86_64-linux.default
+    adwaita-icon-theme
+    gnome-themes-extra
     bat
-    mesa
+    bzip2
+    cargo
+    cmake
+    diff-so-fancy
+    eza
     foot
-    kitty
+    fzf
+    gcc
+    gdbm
     ghostty
+    git
+    gnumake
+    kitty
+    libffi
+    mesa
+    nautilus
+    ncurses
+    neofetch
+    nodejs
+    oh-my-fish
+    openssl
+    patch
+    pyenv
+    python3
+    python3Packages.pip
+    python3Packages.setuptools
+    python3Packages.wheel
+    qt6Packages.qt6ct
+    readline
+    ripgrep
+    rofi
+    rustc
+    sqlite
+    telegram-desktop
+    tk
     waybar
     wofi
-    oh-my-fish
-    yadm
     xclip
+    xz
+    yadm
+    zig
+    zlib
+    zoxide
   ];
 
   nixpkgs.config.allowUnfree = true;
@@ -64,9 +126,161 @@
   fonts.packages = with pkgs; [
     nerd-fonts.fira-code
   ];
-  
-  programs.firefox.enable = true;
 
+  # GTK
+
+  environment.variables = {
+    GTK_THEME = "Adwaita:dark";
+    GDK_DARK_MODE = "1"; # GTK4 dark mode
+  };
+
+  environment.etc."gtk-3.0/settings.ini".text = ''
+    [Settings]
+    gtk-theme-name = Adwaita-dark
+    gtk-application-prefer-dark-theme = 1
+  '';
+
+  environment.etc."gtk-4.0/settings.ini".text = ''
+    [Settings]
+    gtk-theme-name = Adwaita-dark
+    gtk-application-prefer-dark-theme = 1
+  '';
+
+  environment.variables = {
+    QT_QPA_PLATFORMTHEME = "qt5ct";
+  };
+
+  # Stylix
+
+  stylix = {
+    enable = true;
+    autoEnable = true;
+    polarity = "dark";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+    image = "/home/exdis/wp.jpg";
+  };
+
+  # Kanata
+
+  systemd.services.kanata-internalKeyboard.serviceConfig = {
+    SupplementaryGroups = [
+      "input"
+      "uinput"
+    ];
+  };
+
+  services.kanata = {
+    enable = true;
+    keyboards = {
+      internalKeyboard = {
+        devices = [
+	  "/dev/input/by-path/pci-0000:74:00.4-usb-0:2.3:1.1-event-kbd"
+	  "/dev/input/by-path/pci-0000:74:00.4-usb-0:2.4:1.0-event-kbd"
+	  "/dev/input/by-path/pci-0000:74:00.4-usb-0:2.4:1.2-event-kbd"
+        ];
+	port = 1234;
+        extraDefCfg = "process-unmapped-keys yes danger-enable-cmd yes";
+        config = ''
+(defsrc
+  f1   f2   f3   f4   f5   f6   f7   f8   f9   f10   f11   f12
+  grv  1    2    3    4    5    6    7    8    9    0    -    =    bspc
+  tab  q    w    e    r    t    y    u    i    o    p    [    ]    \
+  caps a    s    d    f    g    h    j    k    l    ;    '    ret
+  lsft z    x    c    v    b    n    m    ,    .    /    rsft
+  lctl lmet lalt           spc            ralt rmet rctl
+)
+
+(defvar
+  tap-time 200
+  hold-time 200
+
+  left-hand-keys (
+    q w e r t
+    a s d f g
+    z x c v b
+  )
+  right-hand-keys (
+    y u i o p
+    h j k l ;
+    n m , . /
+    )
+)
+(deflayer us
+  brdn  brup  _    _    _    _   prev  pp  next  mute  vold  volu
+  grv     1    2    3    4    5    6    7    8    9    0    -    =    bspc
+  tab     q    w    e    r    t    y    u    i    o    p    [    ]    \
+  esc     @a   @s   @d   @f   g    h    @j   @k   @l   @;    '    ret
+  @lshift z    x    c    v    b    n    m    ,    .    /    @rshift
+  lctl @lmet lalt           spc            ralt @rmet rctl
+)
+
+(deflayer nomods
+  brdn  brup  _    _    _    _   prev  pp  next  mute  vold  volu
+  grv     1    2    3    4    5    6    7    8    9    0    -    =    bspc
+  tab     q    w    e    r    t    y    u    i    o    p    [    ]    \
+  esc     a    s    d    f    g    h    j    k    l    ;    '    ret
+  @lshift z    x    c    v    b    n    m    ,    .    /    @rshift
+  lctl @lmet lalt           spc            ralt @rmet rctl
+)
+
+(deflayer numpad
+  brdn  brup  _    _    _    _   prev  pp  next  mute  vold  volu
+  grv     1    2    3    4    5    6    7    8    9    0    -    =    bspc
+  tab     1    2    3    4    t    y    u    i    o    p    [    ]    \
+  esc     5    6    7    8    g    h    j    k    l    ;    '    ret
+  @lshift -    =    9    0    b    n    m    ,    .    /    @rshift
+  lctl @lmet lalt           spc            ralt @rmet rctl
+)
+
+(deflayer nav
+  brdn  brup  _    _    _    _   prev  pp  next  mute  vold  volu
+  grv     1    2    3    4    5    6    7    8    9    0    -    =    bspc
+  tab     q    w    e    r    t    y    u    i    o    p    bspc    ]    \
+  esc     a    s    d    f    g    left down up   right ;    '    ret
+  @lshift z    x    c    v    b    n    m    ,    .    /    @rshift
+  lctl @lmet lalt           spc            ralt @rmet rctl
+)
+
+(defalias
+  npad (layer-while-held numpad)
+)
+
+(defalias
+  nnav (layer-while-held nav)
+)
+
+(deffakekeys
+  to-us (layer-switch us)
+)
+
+(defalias
+  tap (multi
+    (layer-switch nomods)
+    (on-idle-fakekey to-us tap 20)
+  )
+
+  rmet @npad
+  lmet @nnav
+
+  a (tap-hold-release-keys $tap-time $hold-time (multi a @tap)  lctl $left-hand-keys)
+  s (tap-hold-release-keys $tap-time $hold-time (multi s @tap) @lmet $left-hand-keys)
+  d (tap-hold-release-keys $tap-time $hold-time (multi d @tap)  lsft $left-hand-keys)
+  f (tap-hold-release-keys $tap-time $hold-time (multi f @tap)  lmet $left-hand-keys)
+  j (tap-hold-release-keys $tap-time $hold-time (multi j @tap)  rmet $right-hand-keys)
+  k (tap-hold-release-keys $tap-time $hold-time (multi k @tap)  rsft $right-hand-keys)
+  l (tap-hold-release-keys $tap-time $hold-time (multi l @tap) @rmet $right-hand-keys)
+  ; (tap-hold-release-keys $tap-time $hold-time (multi ; @tap)  rctl $right-hand-keys)
+
+  lshift (tap-hold-press $tap-time $hold-time C-A-\ lsft)
+  rshift (tap-hold-press $tap-time $hold-time C-S-A-\ rsft)
+)
+	'';
+      };
+    };
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
   system.stateVersion = "25.05";
 }
 
--- .nixos/flake.nix
+++ .nixos/flake.nix
@@ -1,15 +1,21 @@
-            {
-  inputs = {
-    # This is pointing to an unstable release.
-    # If you prefer a stable release instead, you can this to the latest number shown here: https://nixos.org/download
-    # i.e. nixos-24.11
-    # Use `nix flake update` to update the flake to the latest revision of the chosen release channel.
+{
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = inputs@{ self, nixpkgs, ... }: {
-    # NOTE: 'nixos' is the default hostname
+  outputs = { nixpkgs, ... } @inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      modules = [ ./configuration.nix ];
+      modules = [
+        inputs.stylix.nixosModules.stylix
+        ./configuration.nix
+      ];
+      specialArgs = { inherit inputs; };
     };
   };
 }
