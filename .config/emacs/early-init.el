b9aef5318535f7d993c4fe01f2a3946f63b0738e
Denis Kolesnikov <exdis@proton.me>
2026-04-17 10:46:56 +0200

    Emacs modifier keys adjustments

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,10 +1,15 @@
 ;; Window bar
 (add-to-list 'default-frame-alist '(undecorated . t))
 
-;; macOS modifier keys: use Command as Meta, Option as Super
+;; macOS modifier keys:
+;; Left Cmd  -> Super (native mac shortcuts: Cmd+V, Cmd+C, ...)
+;; Right Cmd -> Meta  (M-x etc.)
+;; Option    -> none  (let kanata layers pass through untouched)
 (when (eq system-type 'darwin)
-  (setq mac-command-modifier 'meta)
-  (setq mac-option-modifier 'super))
+  (setq mac-command-modifier       'super)
+  (setq mac-right-command-modifier 'meta)
+  (setq mac-option-modifier        'none)
+  (setq mac-right-option-modifier  'none))
 
 ;; Fonts
 (add-to-list 'default-frame-alist '(font . "FiraCode Nerd Font-12"))
